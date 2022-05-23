6ee3d1a3f326de26bae8a48d88a4a9551d0b11c5
GaneSh GFX <43039255+ganeshgfx@users.noreply.github.com>
2022-05-23 18:40:35 +0530

    rsa

--- practical_5/code.c
+++ practical_5/code.c
@@ -1,26 +1,60 @@
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-void main()
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+int gcd(int a, int b)
 {
-	char p[30],c[30],k[30];
-	int i,j=0,len;
-	
-	printf("Enter PlainText ==> ");
-	gets(p);
-	printf("Enter Key ==> ");
-	gets(k);
-	len=strlen(k);
-	for(i=0;p[i]!='\0';i++)
+	int i, c;
+	for (i = 1; i <= a && i <= b; i++)
 	{
-		c[i]=((p[i]+k[j])%26)+'a';
-		j++;
-		if(j==len)
+		if (a % i == 0 && b % i == 0)
 		{
-			j=0;
+			c = i;
 		}
 	}
-	c[i]='\0';
-	printf("\nCipher text is %s",c);
+	return c;
+}
+void main()
+{
+	int p, q, n, f, e, d, s, msg;
+	long enc, dec;
+	clrscr();
+	printf("Enter 1st prime number ==> ");
+	scanf("%d", &p);
+	printf("Enter 2nd prime number ==> ");
+	scanf("%d", &q);
+	n = p * q;
+	f = (p - 1) * (q - 1);
+	printf("\nn ==> %d", n);
+	printf("\nf ==> %d", f);
+	printf("\n\nEnter public key e ==> ");
+	scanf("%d", &e);
+	while (e < f)
+	{
+		if (gcd(e, f) == 1)
+		{
+			break;
+		}
+		else
+		{
+			e++;
+		}
+	}
+	d = 1;
+	do
+	{
+		s = (d * e) % f;
+		d++;
+	} while (s != 1);
+	d = d - 1;
+	printf("\nPublic Key is {%d,%d}", e, n);
+	printf("\nPrivate Key is {%d,%d}", d, n);
+	printf("\n\nEnter your Messege ==> ");
+	scanf("%d", &msg);
+	enc = pow(msg, e);
+	enc = fmod(enc, n);
+	dec = pow(enc, d);
+	dec = fmod(dec, n);
+	printf("\nEncryption ==> %ld", enc);
+	printf("\nDecryption ==> %ld", dec);
 	getch();
 }
